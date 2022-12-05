43ac22ae18c5e343c5ad9176ed5b7c15e0809aba
eduardo <tato87911@gmail.com>
2022-12-04 18:05:08 -0600

    cambios

---
+++ Semana16-2/Controllers/HurtController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana16_2.Controllers
+{
+    public class HurtController
+    {
+    }
+}
